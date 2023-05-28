318bc47f93bcad35083f8d6b47317b395bcf06ed
Egwuenu Precious <egwuenuprecious7@gmail.com>
2023-05-28 14:52:13 +0100

    fix: single page bug

--- src/pages/photos/[id].js
+++ src/pages/photos/[id].js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { getPhotosById } from "../../../lib/api";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { FiMoreHorizontal, FiShare } from "react-icons/fi";
 import { GrLink } from "react-icons/gr";
 import { Dropdown, Popover } from "antd";
@@ -18,7 +19,6 @@ import {
 } from "react-share";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import toast, { Toaster } from "react-hot-toast";
-import { useRouter } from 'next/router';
 import MyComponent from "../../../components/Comments";
 
 export async function getServerSideProps({ params }) {
@@ -31,7 +31,9 @@ export async function getServerSideProps({ params }) {
   };
 }
 
-export default function photoId({ pic }) {
+export default function PhotoId({ pic }) {
+  // go back function
+  const router = useRouter();
 
   // To download the image
   const downloadImage = () => {
@@ -102,7 +104,6 @@ export default function photoId({ pic }) {
             <CopyToClipboard text={shareUrl} onCopy={notify}>
               <GrLink className="text-xl" />
             </CopyToClipboard>
-            <Toaster />
           </div>
           <p>Copy link</p>
         </div>
@@ -113,89 +114,85 @@ export default function photoId({ pic }) {
   // save to collections
   const notification = () => toast.success("Saved to collections!");
 
-  // go back function
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const router = useRouter();
-
-
   return (
     <div className="h-screen items-center justify-center">
+      <button
+        type="button"
+        onClick={() => router.back()}
+        className="p-5 underline"
+      >
+        Go back
+      </button>
 
-    <button type="button" onClick={() => router.back()} className="p-5">
-       Go back
-    </button>
-
-      <div className="flex items-center justify-center">
-       <div className="w-[85%] mx-auto grid lg:flex justify-center">
-        <div className="flex items-center justify-center">
-          <div
-            download={`${pic.alt}.jpg`}
-            className="cursor-pointer lg:w-[500px] md:w-[500px] h-[600px] relative w-[320px] shadow-lg"
-          >
-            <Image
-              src={pic.src.portrait}
-              alt={pic.alt}
-              fill
-              sizes="300"
-              priority
-              className="object-cover lg:rounded-tl-[30px] lg:rounded-bl-[30px] rounded-tl-[20px] rounded-tr-[20px] lg:rounded-tr-[0px] shadow-lg"
-              style={{ backgroundColor: pic.avg_color }}
-            />
-          </div>
-        </div>
-        <div className="lg:w-[500px] md:w-[500px] w-[320px] h-[600px] overflow-auto  p-5 bg-[#fffff]  lg:rounded-tr-[30px] lg:rounded-br-[30px] lg:rounded-bl-[0px] rounded-bl-[20px] rounded-br-[20px] shadow-lg">
-          <div className="flex justify-between items-center">
-            <div className="flex justify-around">
-              <span className="text-[19px] font-[800] text-[#111111] transition-all duration-300 cursor-pointer h-12 w-12 hover:bg-gray-100 flex justify-center items-center rounded-[50px]">
-                <Dropdown
-                  menu={{
-                    items,
-                  }}
-                  placement="bottom"
-                  trigger={["click"]}
-                >
-                  <span onClick={(e) => e.preventDefault()}>
-                    <FiMoreHorizontal />
-                  </span>
-                </Dropdown>
-              </span>
-              <span className="text-[19px] font-[800] text-[#111111] transition-all duration-300 cursor-pointer h-12 w-12 hover:bg-gray-100 flex justify-center items-center rounded-[50px]">
-                <Popover placement="bottom" content={content}>
-                  <FiShare />
-                </Popover>
-              </span>
-              <span className="text-[19px] font-[800] text-[#111111] transition-all duration-300 cursor-pointer h-12 w-12 hover:bg-gray-100 flex justify-center items-center rounded-[50px]">
-                <CopyToClipboard text={shareUrl} onCopy={notify}>
-                  <GrLink />
-                </CopyToClipboard>
-                <Toaster />
-              </span>
-            </div>
-            <button
-              onClick={notification}
-              className="hover:bg-[#E60023] bg-[red] text-[14px] text-white h-[48px] p-[12px] rounded-[64px] w-[68px]"
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-[85%] mx-auto grid lg:flex justify-center">
+          <div className="flex items-center justify-center">
+            <div
+              download={`${pic.alt}.jpg`}
+              className="cursor-pointer lg:w-[500px] md:w-[500px] h-[600px] relative w-[320px] shadow-lg lg:rounded-tl-[30px] lg:rounded-bl-[30px] rounded-tl-[20px] rounded-tr-[20px] lg:rounded-tr-[0px]"
             >
-              Save
-            </button>
-            <Toaster />
+              <Image
+                src={pic.src.portrait}
+                alt={pic.alt}
+                fill
+                sizes="300"
+                priority
+                className="object-cover lg:rounded-tl-[30px] lg:rounded-bl-[30px] rounded-tl-[20px] rounded-tr-[20px] lg:rounded-tr-[0px]  shadow-lg"
+                style={{ backgroundColor: pic.avg_color }}
+              />
+            </div>
           </div>
-          <p className="lg:text-[38px] font-600">{pic.alt}</p>
-          <Link href={pic.photographer_url}>
-            <p className="cursor-pointer underline text-x">
-              {pic.photographer}
-            </p>
-          </Link>
+          <div className="lg:w-[500px] md:w-[500px] w-[320px] h-[600px] overflow-auto  p-5 bg-[#fffff]  lg:rounded-tr-[30px] lg:rounded-br-[30px] lg:rounded-bl-[0px] rounded-bl-[20px] rounded-br-[20px] shadow-lg">
+            <div className="flex justify-between items-center">
+              <div className="flex justify-around">
+                <span className="text-[19px] font-[800] text-[#111111] transition-all duration-300 cursor-pointer h-12 w-12 hover:bg-gray-100 flex justify-center items-center rounded-[50px]">
+                  <Dropdown
+                    menu={{
+                      items,
+                    }}
+                    placement="bottom"
+                    trigger={["click"]}
+                  >
+                    <span onClick={(e) => e.preventDefault()}>
+                      <FiMoreHorizontal />
+                    </span>
+                  </Dropdown>
+                </span>
+                <span className="text-[19px] font-[800] text-[#111111] transition-all duration-300 cursor-pointer h-12 w-12 hover:bg-gray-100 flex justify-center items-center rounded-[50px]">
+                  <Popover placement="bottom" content={content}>
+                    <FiShare />
+                  </Popover>
+                </span>
+                <span className="text-[19px] font-[800] text-[#111111] transition-all duration-300 cursor-pointer h-12 w-12 hover:bg-gray-100 flex justify-center items-center rounded-[50px]">
+                  <CopyToClipboard text={shareUrl} onCopy={notify}>
+                    <GrLink />
+                  </CopyToClipboard>
+                </span>
+              </div>
+              <button
+                onClick={notification}
+                className="hover:bg-[#E60023] bg-[red] text-[14px] text-white h-[48px] p-[12px] rounded-[64px] w-[68px]"
+              >
+                Save
+              </button>
+              <Toaster />
+            </div>
+            <p className="lg:text-[38px] font-600">{pic.alt}</p>
+            <Link href={pic.photographer_url}>
+              <p className="cursor-pointer underline text-x">
+                {pic.photographer}
+              </p>
+            </Link>
 
-          <div className="grid items-start justify-start mt-[5%]">
-            <p className="text-xl mt-4 font-bold">Comments</p>
-            <div className="mt-[5%]">
-              <MyComponent />
+            <div className="grid items-start justify-start mt-[5%]">
+              <p className="text-xl mt-4 font-bold">Comments</p>
+              <div className="mt-[5%]">
+                <MyComponent />
+              </div>
             </div>
           </div>
         </div>
-      </div> 
       </div>
-      
     </div>
   );
 }
